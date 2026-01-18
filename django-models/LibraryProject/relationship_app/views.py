e9d9bf3f6d4807c17b339ae596b27bceccf0432c
Omar <omartarek680@gmail.com>
2026-01-18 15:09:43 +0200

    Update files

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 # relationship_app/views.py
 from django.shortcuts import render
-from .models import Book, Library
-from django.views.generic import ListView, DetailView
+from .models import Book
+from .models import Library
+from django.views.generic import  DetailView
 
 
 def list_books(request):
