ede69874791b2b04663ac0baf8605871faf9b956
Omar <omartarek680@gmail.com>
2026-01-18 14:25:23 +0200

    Update files

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,12 @@
 from relationship_app.models import Author, Book, Library, Librarian
-omar = Author.objects.get(name='Omar')
+omar = Author.objects.get(name='author_name')
 books = Book.objects.filter(author=omar)
 for book in books:
     print(book.title)
 
-
-library = Library.objects.get(name='library_name')
-books = library.books.all()
+books = Book.objects.filter(libraries__name='library_name')
 for book in books:
     print(book.title)
-
 library = Library.objects.get(name='library_name')
 librarian = Librarian.objects.get(library=library)
 print(librarian.name)
