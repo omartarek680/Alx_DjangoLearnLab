97715715065ee1204064c0a7cdd843a521a8ccf5
Omar <omartarek680@gmail.com>
2026-01-22 14:51:57 +0200

    UUpdates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('librarian_dashboard/', views.librarian_dashboard, name='librarian_dashboard'),
     path('member_dashboard/', views.member_dashboard, name='member_dashboard'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
