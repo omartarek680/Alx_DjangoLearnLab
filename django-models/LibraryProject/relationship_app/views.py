35d9b2c0871f98a54c3a6f44f40d808c12b84f0f
Omar <omartarek680@gmail.com>
2026-01-22 14:20:22 +0200

    UUpdates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -55,16 +55,16 @@ def is_member(user):
     return user.is_authenticated and user.userprofile.role == 'Member'
 
 @login_required
-@user_passes_test(lambda user: user.is_authenticated and user.userprofile.role == 'Admin')
-def admin_dashboard(request):
+@user_passes_test(is_admin)
+def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @login_required
 @user_passes_test(is_librarian)
-def librarian_dashboard(request):
+def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 @login_required
 @user_passes_test(is_member)
-def member_dashboard(request):
+def member_view(request):
     return render(request, 'relationship_app/member_view.html')
