4af45b87664df5fc6f624f617e0cd2bfb58b5e0b
Omar <omartarek680@gmail.com>
2026-01-25 14:32:35 +0200

    Add

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-from .models import Book
+from .models import Book, CustomUser
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author','publication_year')
     list_filter = ('publication_year',)
+
+@admin.register(CustomUser,UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_of_birth', 'favorite_genre')
+    search_fields = ('username', 'email')
+    list_filter = ('date_of_birth',)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
@@ -6,4 +7,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)  
 
