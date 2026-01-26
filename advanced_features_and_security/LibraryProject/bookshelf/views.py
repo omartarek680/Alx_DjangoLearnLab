e1614d408fdd462fbe8db507e3b8548ebe53984f
Omar <omartarek680@gmail.com>
2026-01-26 15:54:50 +0200

    Security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-
+from .forms import ExampleForm
 # Create your views here.
 
 @permission_required('bookshelf.can_add', raise_exception=True)
@@ -15,4 +15,13 @@ def book_list_view(request):
 
 
 
-def 
+def form_example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            return render(request, 'bookshelf/form_success.html', {'name': name, 'email': email})
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
