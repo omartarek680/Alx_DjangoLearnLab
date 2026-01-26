34637add7cc6c6813f5a65a2f2d66a88b15b3efb
Omar <omartarek680@gmail.com>
2026-01-26 15:40:10 +0200

    Permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+
+@permission_required('bookshelf.can_add', raise_exception=True)
+def edit_view(request):
+    return render(request, 'bookshelf/edit.html')
