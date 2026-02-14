1e1da9756dc952ac22ee97139fe8fcb9edb79977
= <=>
2026-02-14 08:30:20 +0200

    Update URLs

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("books/", views.ListView.as_view(), name="book-list"),
     path("books/create/", views.CreateView.as_view(), name="book-create"),
     path("books/<int:pk>/", views.DetailView.as_view(), name="book-detail"),
-    path("books/<int:pk>/delete/", views.DeleteView.as_view(), name="book-delete"),
-    path("books/<int:pk>/update/", views.UpdateView.as_view(), name="book-update"),
+    path("books/delete/<int:pk>/", views.DeleteView.as_view(), name="book-delete"),
+    path("books/update/<int:pk>/", views.UpdateView.as_view(), name="book-update"),
 ]
