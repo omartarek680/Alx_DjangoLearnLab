70e4f21206828eafef638b89a3430a7600842012
= <=>
2026-02-17 10:40:26 +0200

    Add Post and Comments

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -11,3 +11,18 @@ class CustomUserModel(AbstractUser):
 		related_name="following",          # reverse name
         related_query_name="following", 
     )
+
+
+class Post(models.Model):
+	author = models.ForeignKey(CustomUserModel,on_delete=models.CASCADE, related_name='authored_posts')
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+	author = models.ForeignKey(CustomUserModel, on_delete=models.CASCADE, related_name='comments')
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
